8ee7c7e8c3f1694f19d1e7dcb80be79e9edf1849
Claudio Schaepper <claudio.schaepper@cec.valantic.com>
2020-03-30 17:22:32 +0200

    Adds 404 error page

--- src/views/Error.vue
+++ src/views/Error.vue
@@ -1,6 +1,17 @@
 <template>
   <div :class="b()">
-    <h1>404</h1>
+    <h1 :class="b('code')">
+      404
+    </h1>
+    <p :class="b('text')">
+      Die gesuchte Seite wurde leider nicht gefunden.
+    </p>
+    <a :class="b('link')" href="/">
+      zur Startseite
+    </a>
+    <img :class="b('img')"
+         src="../assets/fish_404.png"
+         alt="Toter Fisch">
   </div>
 </template>
 
@@ -36,5 +47,33 @@
 
 <style lang="scss">
   .error {
+    position: relative;
+    width: 100%;
+
+    &__code {
+      font-size: 80px;
+      font-weight: $font-weight--bold;
+      color: $color-primary--1;
+      z-index: 1;
+      position: relative;
+
+      @include media(sm) {
+        font-size: 200px;
+      }
+
+      @include media(lg) {
+        font-size: 300px;
+      }
+
+    }
+
+    &__img {
+      position: absolute;
+      top: 50%;
+      right: 0;
+      filter: grayscale(100%);
+      opacity: 0.05;
+      max-width: 100%;
+    }
   }
 </style>
